b558f8ae7398fe9f077bb39a02165fa3086aa633
Cesar Bautista <cesarbtta06@gmail.com>
2016-06-27 19:00:18 -0500

    Se cambia el enum del estado de la bateria

--- src/main/java/com/mx/kiibal/celsales/domain/enumeration/EstadoBateria.java
+++ src/main/java/com/mx/kiibal/celsales/domain/enumeration/EstadoBateria.java
@@ -4,11 +4,17 @@ package com.mx.kiibal.celsales.domain.enumeration;
  * The EstadoBateria enumeration.
  */
 public enum EstadoBateria {
-    BATTERY_HEALTH_COLD,
+    /*BATTERY_HEALTH_COLD,
     BATTERY_HEALTH_DEAD,
     BATTERY_HEALTH_GOOD,
     BATTERY_HEALTH_OVER_VOLTAGE,
     BATTERY_HEALTH_OVERHEAT,
     BATTERY_HEALTH_UNSPECIFIED_FAILURE,
-    BATTERY_HEALTH_UNKNOWN
+    BATTERY_HEALTH_UNKNOWN*/
+    Cold,
+    Dead,
+    Good,
+    Over_Voltage,
+    Overheat,
+    Unspecified
 }
